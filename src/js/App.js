f4d77ce4279fe0b13e52fd5d46da3bf5dcd3080f
fergus <fergus.jingtai@gmail.com>
2017-12-27 02:36:40 +0800

    3. Card details rendering

--- src/js/App.js
+++ src/js/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
 
   }
   render(){
-		const { properties } = this.state;
+		const { properties, activeProperty } = this.state;
 
     return (
       <div>
@@ -102,7 +102,11 @@ class App extends React.Component {
               
 							{
 								properties.map(property => {
-									return <Card property={property} />
+									return <Card
+										key={property._id}
+										property={property}
+										activeProperty={activeProperty}
+									/>
 								})
 							}
 
